b548d20abbfbb4ff1887178ebc819440e3dde339
Carlos Mario Hernández León <84859398+CharloHdz@users.noreply.github.com>
2024-10-06 23:08:13 -0600

    .

--- Assets/quique/Scripts/GridData.cs
+++ Assets/quique/Scripts/GridData.cs
@@ -44,9 +44,9 @@ public class GridData
         foreach (var pos in positionToOccupy)
         {
             if (!placedObjects.ContainsKey(pos))
-                return true;
+                return false;
         }
-        return false;
+        return true;
     }
 
 }
