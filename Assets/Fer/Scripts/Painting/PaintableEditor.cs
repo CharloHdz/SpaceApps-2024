b96963ecfe85fce1e77f7bcb0dce58ab1b9ede12
Fernando Mendoza <fernandogx8647@gmail.com>
2024-10-07 00:00:27 -0600

    Fix

--- Assets/Fer/Scripts/Painting/PaintableEditor.cs
+++ Assets/Fer/Scripts/Painting/PaintableEditor.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -24,3 +24,4 @@ public class PaintableEditor : Editor
         }
     }
 }
+*/
